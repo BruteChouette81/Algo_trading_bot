03e0bd5f2fe9ab3cde022bed995f7b5202090257
Thomas Berthiaume <hbaril1@icloud.com>
2021-07-23 11:11:52 -0400

    formating and tuning the strategy

--- main.py
+++ main.py
@@ -4,13 +4,43 @@ import plotly.graph_objects as go
 import plotly.express as px
 import time
 import pandas as pd
+import json
+kraken = krakenex.API()
+k = KrakenAPI(kraken)
+
+MATIME = 10
+ADTVTIME = 30
+BALANCE = 100
+stock = 'BTCUSD'
+data = {
+        'ma' : [],
+        'prices' : []
+        }
+
+def create_trades(time, buy, sell, stock, price):
+    trades = {"time": time,
+    "buy": buy,
+    "sell": sell,
+    "stock": stock,
+    "price": price
+    }
+    return trades
+
+def create_file(trades):
+    json_object = json.dumps(trades)
+    with open("trades.json", "w") as file:
+        file.write(json_object)
+
 
 #calculate the actual average daily trading volume
 def adtv(volume, time_laps):
     counter_average = 0
     for vol in volume[-time_laps:]:
-        counter_average += float(vol[0])
+        if counter_average == 0:
+            counter_average = float(vol[0])
 
+        else:
+            counter_average += float(vol[0])
     return counter_average / time_laps
 
 # verify if the volume is higher than the average volume
@@ -22,115 +52,110 @@ def verify_adtv(adtv, live_volume):
     else:
         return False
 
-kraken = krakenex.API()
-k = KrakenAPI(kraken)
-data = {
-        'ma' : [],
-        'prices' : []
-        }
+def request_data(stock):
+    times = str(int(time.time() - 43200))
+    ohlc = k.get_ohlc_data(stock, since=times, ascending=True)
+    return ohlc
+
+def calculate_indicator(ohlc, MATIME, ADTVTIME):
+    vol = ohlc[0][["volume"]]
+    print(vol)
+    #calculate the 30 minute volume average
+    av = adtv(vol.values.tolist(), ADTVTIME)
+    list_vol = vol.values.tolist()
+    live_vol = float(list_vol[-1:][0][0])
+    print(live_vol)
 
 
-times = str(int(time.time() - 43200))
-ohlc = k.get_ohlc_data('BTCUSD', since=times, ascending=True)
+    btc = ohlc[0][["close"]]
 
-vol = ohlc[0][["volume"]]
-print(vol)
-#calculate the 30 minute volume average
-av = adtv(vol.values.tolist(), 30)
-list_vol = vol.values.tolist()
-live_vol = float(list_vol[-1:][0][0])
-print(live_vol)
+    btc.rename(columns={'close':'Price'}, inplace=True)
+    btc["ma"] = btc["Price"].rolling(window=MATIME).mean()
 
+    return btc, av, live_vol
 
-btc = ohlc[0][["close"]]
-
-btc.rename(columns={'close':'Price'}, inplace=True)
-btc["100ma"] = btc["Price"].rolling(window=100).mean()
-btc["13bar"] = btc["Price"].rolling(window=13).mean()
-btc["8bar"] = btc["Price"].rolling(window=8).mean()
-btc["5bar"] = btc["Price"].rolling(window=5).mean()
+def draw(ohlc, btc):
+    fig = go.Figure(data = [go.Candlestick(x = ohlc[0].index,
+                                            open = ohlc[0]['open'],
+                                            high = ohlc[0]['high'],
+                                            low = ohlc[0]['low'],
+                                            close = ohlc[0]['close'],
+                                            ),
+                            go.Scatter(x = ohlc[0].index, y = btc['ma'], line=dict(color = 'red', width = 1))])
+    fig.show()
 
 
-fig = go.Figure(data = [go.Candlestick(x = ohlc[0].index,
-                                        open = ohlc[0]['open'],
-                                        high = ohlc[0]['high'],
-                                        low = ohlc[0]['low'],
-                                        close = ohlc[0]['close'],
-                                        ),
-                        go.Scatter(x = ohlc[0].index, y = btc['100ma'], line=dict(color = 'red', width = 1))])
+def algo(btc):
+    ma = btc["ma"].values.tolist()
+    price = btc["Price"].values.tolist()
+    data["ma"].append(ma)
+    data["prices"].append(price)
 
-fig.add_trace(go.Scatter(x = ohlc[0].index, y = btc['8bar'], line=dict(color = 'green', width = 1)))
+    ma_10min_data = data["ma"][0][-10:] # reversed 
+    price_10min_data = [price for price in data["prices"][0][-10:]] #reversed(data["prices"][0][-10:])
+    print(price_10min_data)
+    print(ma_10min_data)
 
-fig.add_trace(go.Scatter(x = ohlc[0].index, y = btc['13bar'], line=dict(color = 'purple', width = 1)))
+    trend = []
+    counter = 0
+    for i in range(0, 10):
+        if ma_10min_data[i] < price_10min_data[i]:
+            trend.append("uptrend")
+            if counter == 0:
+                counter = 1
 
-fig.show()
+            else:
+                counter += 1
 
-
-ma = btc["8bar"].values.tolist()
-price = btc["Price"].values.tolist()
-data["ma"].append(ma)
-data["prices"].append(price)
-
-ma_10min_data = data["ma"][0][-10:] # reversed 
-price_10min_data = [price for price in data["prices"][0][-10:]] #reversed(data["prices"][0][-10:])
-print(price_10min_data)
-print(ma_10min_data)
-
-trend = []
-save_price = []
-counter = 0
-for i in range(0, 10):
-    if ma_10min_data[i] < price_10min_data[i]:
-        trend.append("uptrend")
-        if counter == 0:
-            counter = 1
-
+        elif ma_10min_data[i] > price_10min_data[i]:
+            trend.append("downtrend")
+            counter - 1
+        
         else:
-            counter += 1
+            trend.append("not_trend")
+            continue
 
-    elif ma_10min_data[i] > price_10min_data[i]:
-        trend.append("downtrend")
-        counter - 1
-    
+    print(trend)
+
+    return trend, counter
+
+
+def verify_transaction(av, live_vol, counter):
+    volume_trend = verify_adtv(av, live_vol)
+    print(av)
+    print(live_vol)
+    if counter >= 7 and volume_trend:
+        print("buy")
+        return True
+
+
+    elif counter < 7 and volume_trend:
+        print("sell")
+        return False
+
     else:
-        trend.append("not_trend")
-        continue
-
-print(trend)
-
-volume_trend = verify_adtv(av, live_vol)
-print(av)
-print(live_vol)
-if counter >= 7 and volume_trend:
-    print("buy")
-
-
-elif counter < 7 and volume_trend:
-    print("sell")
+        return None
 
 
 #trading crypto bot
+def bot():
+    req = request_data(stock)
+    btc, av, live_vol = calculate_indicator(req, MATIME=MATIME, ADTVTIME=ADTVTIME)
+    draw(req, btc)
+    trend, counter = algo(btc)
+    transaction = verify_transaction(av, live_vol, counter)
+    print(transaction)
+    
 
-BALANCE = 10
-
-def get_live_prize():
-    pass
 
 
-class trend_following:
-    def __init__(self, data):
-        self.data = data
 
-    def algo(self):
-        for i in range(self.data):
-            if self.data["13bar"].iloc[i] > self.data["Price"].iloc[i]:
-                print("buy")
+#trend is up and real price is higher than the moving average == up trend = BUY
+#real price is same or lass than the average price == down trend = sell
 
-            else:
-                print("sell")
+if __name__ == '__main__':
+    bot = bot()
 
-    #trend is up and real price is higher than the moving average == up trend = BUY
-    #real price is same or lass than the average price == down trend = sell
 
 
 '''
